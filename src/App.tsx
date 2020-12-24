d2556e24881391848a8abed09cdbc045788a9a2a
Gabriel Jose de Oliveira <gaoliveira211999@gmail.com>
2020-12-24 17:33:58 -0300

    refactor: use calendar component on datepicker

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import React, { ReactElement } from 'react'
 
 import { DatePicker } from './components/DatePicker'
-import { Calendar } from './components/Calendar'
+// import { Calendar } from './components/Calendar'
 
 import css from './App.sass'
 
@@ -12,10 +12,10 @@ export const App: React.FC = (): ReactElement => {
         <h2>Datepicker</h2>
         <DatePicker />
       </div>
-      <div className={css.A__Wrapper}>
+      {/* <div className={css.A__Wrapper}>
         <h2>Calendar</h2>
         <Calendar />
-      </div>
+      </div> */}
     </div>
   )
 }
--- src/components/DatePicker/index.tsx
+++ src/components/DatePicker/index.tsx
@@ -1,36 +1,15 @@
 import React, { ReactElement } from 'react'
 
-import { CalendarProvider } from '@/context/calendar'
-
-import { Header } from './Header'
-import { Body } from './Body'
+import { Calendar } from '@/components/Calendar'
 import { Footer } from './Footer'
-import { Input } from './Input'
 
 import css from './DatePicker.sass'
 
 export const DatePicker: React.FC = (): ReactElement => {
   return (
-    <CalendarProvider>
-      <Input />
-      <div className={css.Calendar}>
-        <Header />
-        <main>
-          <header>
-            <ul className={css.C__WeekDays}>
-              <li>Su</li>
-              <li>Mo</li>
-              <li>Tu</li>
-              <li>We</li>
-              <li>Th</li>
-              <li>Fr</li>
-              <li>Sa</li>
-            </ul>
-          </header>
-          <Body />
-        </main>
-        <Footer />
-      </div>
-    </CalendarProvider>
+    <div className={css.DatePicker}>
+      <Calendar />
+      <Footer />
+    </div>
   )
 }
