6ad78420d4a24522ca663c3e3efe5956faba0620
Gabriel Jose de Oliveira <gaoliveira211999@gmail.com>
2020-12-24 10:33:12 -0300

    feat: set date

--- src/components/Day/index.tsx
+++ src/components/Day/index.tsx
@@ -1,14 +1,22 @@
 import React, { ReactElement } from 'react'
-import { isToday } from 'date-fns'
+import { isToday, isEqual } from 'date-fns'
+
+import { useCalendarProviderContext } from '@/context/calendar/context'
 
 import css from './Day.sass'
 
 export const Day: React.FC<{ date: Date | null }> = ({ date }): ReactElement => {
+  const { setSelectedDate, selectedDate } = useCalendarProviderContext()
   const today = isToday(date)
 
-  console.log(today)
-
   return (
-    date ? <li className={`${css.Day} ${today && css['Day--today']}`}>{date.getDate()}</li> : <li></li>
+    date
+      ? <li
+        onClick={() => setSelectedDate(date)}
+        className={`${css.Day} ${today && css['Day--today']} ${isEqual(date, selectedDate) && css['Day--selected']}`}
+      >
+        {date.getDate()}
+      </li>
+      : <li></li>
   )
 }
