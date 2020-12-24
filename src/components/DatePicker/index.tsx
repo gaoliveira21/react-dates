9f33db1c9c34f8aa9cf52769399854217030c2f4
Gabriel Jose de Oliveira <gaoliveira211999@gmail.com>
2020-12-24 17:34:41 -0300

    feat: add input on datepicker

--- src/components/DatePicker/index.tsx
+++ src/components/DatePicker/index.tsx
@@ -2,12 +2,14 @@ import React, { ReactElement } from 'react'
 
 import { Calendar } from '@/components/Calendar'
 import { Footer } from './Footer'
+import { Input } from './Input'
 
 import css from './DatePicker.sass'
 
 export const DatePicker: React.FC = (): ReactElement => {
   return (
     <div className={css.DatePicker}>
+      <Input />
       <Calendar />
       <Footer />
     </div>
