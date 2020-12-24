9e86dcacc9d4f17f46617fa1d0617c722bcabc41
Gabriel Jose de Oliveira <gaoliveira211999@gmail.com>
2020-12-23 22:39:36 -0300

    feat: add calendar base style

--- src/components/Calendar/index.tsx
+++ src/components/Calendar/index.tsx
@@ -16,14 +16,51 @@ export const Calendar: React.FC = (): ReactElement => {
         </button>
       </header>
       <main>
-        <ul className={css.C__WeekDays}>
-          <li>Su</li>
-          <li>Mo</li>
-          <li>Tu</li>
-          <li>We</li>
-          <li>Th</li>
-          <li>Fr</li>
-          <li>Sa</li>
+        <header>
+          <ul className={css.C__WeekDays}>
+            <li>Su</li>
+            <li>Mo</li>
+            <li>Tu</li>
+            <li>We</li>
+            <li>Th</li>
+            <li>Fr</li>
+            <li>Sa</li>
+          </ul>
+        </header>
+        <ul className={css.C__MonthDays}>
+          <li></li>
+          <li></li>
+          <li className={css.MD__Day}>1</li>
+          <li className={css.MD__Day}>2</li>
+          <li className={css.MD__Day}>3</li>
+          <li className={css.MD__Day}>4</li>
+          <li className={css.MD__Day}>5</li>
+          <li className={css.MD__Day}>6</li>
+          <li className={css.MD__Day}>7</li>
+          <li className={css.MD__Day}>8</li>
+          <li className={css.MD__Day}>9</li>
+          <li className={css.MD__Day}>10</li>
+          <li className={css.MD__Day}>11</li>
+          <li className={css.MD__Day}>12</li>
+          <li className={css.MD__Day}>13</li>
+          <li className={css.MD__Day}>14</li>
+          <li className={css.MD__Day}>15</li>
+          <li className={css.MD__Day}>16</li>
+          <li className={css.MD__Day}>17</li>
+          <li className={css.MD__Day}>18</li>
+          <li className={css.MD__Day}>19</li>
+          <li className={css.MD__Day}>20</li>
+          <li className={css.MD__Day}>21</li>
+          <li className={`${css.MD__Day} ${css['MD__Day--selected']}`}>22</li>
+          <li className={`${css.MD__Day} ${css['MD__Day--today']}`}>23</li>
+          <li className={css.MD__Day}>24</li>
+          <li className={css.MD__Day}>25</li>
+          <li className={css.MD__Day}>26</li>
+          <li className={css.MD__Day}>27</li>
+          <li className={css.MD__Day}>28</li>
+          <li className={css.MD__Day}>29</li>
+          <li className={css.MD__Day}>30</li>
+          <li className={css.MD__Day}>31</li>
         </ul>
       </main>
     </div>
